4dc40434475aceafdb19911b553c3ca2f45acee2
peng.xing <southeast_px@163.com>
2016-10-14 14:48:06 +0800

    tomcat类加载机制

---
+++ src/com/pauu/web/servlets/MyServlet.java
@@ -0,0 +1,29 @@
+package com.pauu.web.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		ClassLoader loader = this.getClass().getClassLoader();
+		while(loader!=null){
+			out.println(loader+"<br>");
+			loader = loader.getParent();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
